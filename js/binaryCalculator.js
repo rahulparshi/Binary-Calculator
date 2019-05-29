e4c9f5f4ce631d01a336b1e86dc1904e71a951aa
rahulparshi <rahul547457@gmail.com>
2019-05-29 20:38:22 +0530

    basic functionality

---
+++ js/binaryCalculator.js
@@ -0,0 +1,42 @@
+const btns = document.querySelector("#btns");
+const res = document.querySelector("#res");
+let op1, op2, operator, result;
+
+function calculate(text) {
+  text = text.split(operator);
+  op1 = Number.parseInt(text[0], 2);
+  op2 = Number.parseInt(text[1], 2);
+  switch (operator) {
+    case "+":
+      result = op1 + op2;
+      break;
+    case "-":
+      result = op1 - op2;
+      break;
+    case "*":
+      result = op1 * op2;
+      break;
+    case "/":
+      result = Number.parseInt(op1 / op2);
+      break;
+  }
+
+  return (+result).toString(2);
+}
+
+const operators = ["+", "-", "*", "/"];
+btns.addEventListener("click", function(e) {
+  const char = e.target.innerHTML;
+
+  if (char === "C") {
+    res.innerHTML = "";
+    operator = null;
+  } else if (char === "=") {
+    res.innerHTML = calculate(res.innerHTML);
+  } else {
+    if (operators.includes(char)) {
+      operator = char;
+    }
+    res.innerHTML += char;
+  }
+});
